92c82d2f94379469258eaf30c7cd7eea1ef83c4f
ziaurjoy <ziaurjoy802@gmail.com>
2021-11-15 19:30:25 +0600

    Registar login and logout API

---
+++ miakinotesplus/urls.py
@@ -0,0 +1,35 @@
+"""miakinotesplus URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+#from reviews.views import ProductViewSet, ImageViewSet
+from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+router = DefaultRouter()
+# router.register(r'product', ProductViewSet, basename='Product')
+# router.register(r'image', ImageViewSet, basename='Image')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('usersapp/', include('usersApp.urls')),
+    
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
